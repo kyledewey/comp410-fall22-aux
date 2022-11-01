3c30232b38e7220d57eccd195f4e05fae87305a5
Kyle Dewey <kyle.dewey@csun.edu>
2022-10-31 17:20:42 -0700

    generator code

---
+++ prolog/gen.pl
@@ -0,0 +1,25 @@
+% op ::= `+` | `-` | `*` | `/`
+% exp ::= INT | exp op exp
+%
+% Op ::= plus | minus | mult | div
+% Exp ::= integer(INT) | binop(Exp1, Op, Exp2)
+
+% op/1
+op(plus).
+op(minus).
+op(mult).
+op(div).
+
+isInteger(1).
+isInteger(2).
+
+% exp/2
+% exp: Bound, Exp
+exp(_, integer(Int)) :-
+    isInteger(Int).
+exp(Bound, binop(Exp1, Op, Exp2)) :-
+    Bound > 0,
+    NewBound is Bound - 1,
+    exp(NewBound, Exp1),
+    op(Op),
+    exp(NewBound, Exp2).
